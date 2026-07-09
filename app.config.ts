e2d26cdde9afc8b39fb02ab220a1e546482c2d7e
kenji <kenji@example.com>
2026-07-09 15:07:51 +0900

    fix: explicitly specify platforms in app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -42,6 +42,7 @@ const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
   version: "1.0.0",
+  platforms: ["ios", "android", "web"],
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
